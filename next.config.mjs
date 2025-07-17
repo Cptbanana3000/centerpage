ac238f3cc0b16aed98c8f31dbd8f6f8da2bed809
joelchhetri <joelchhetri2001@gmail.com>
2025-07-17 22:25:11 +0530

    testing links

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    async rewrites() {
+      return [
+        {
+          source: '/__/auth/:path*',
+          destination: 'https://veritolab-990d9.firebaseapp.com/__/auth/:path*',
+        },
+      ];
+    },
+  };
   
-};
-
-export default nextConfig;
+  export default nextConfig;
